6a8cdd0ff64aca7d863235110ed4b822f6185bd9
Josh Shavers <shaversj@gmail.com>
2023-11-29 07:38:46 -0600

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': "375px",
+        'lg': "1440px"
+      },
+      colors: {
+        "soft-red": "hsl(10, 79%, 65%)",
+        "cyan": "hsl(186, 34%, 60%)",
+        "dark-brown": "hsl(25, 47%, 15%)",
+        "medium-brown": "hsl(28, 10%, 53%)",
+        "cream": "hsl(27, 66%, 92%)",
+        "very-pale-orange": "hsl(33, 100%, 98%)"
+      },
+      fontFamily: {
+
+      }
+    },
+  },
+  plugins: [],
+}
